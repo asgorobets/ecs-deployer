c2a645d5346d1642ed6f3a82cb0bb3bdb013f01f
Alexei Gorobet <asgorobets@gmail.com>
2018-01-23 17:02:02 -0800

    Add status command

--- deploy.py
+++ deploy.py
@@ -8,6 +8,8 @@ import subprocess
 import yaml
 
 elbclient = boto3.client('elbv2')
+ecsclient = boto3.client('ecs')
+ec2client = boto3.client('ec2')
 
 def get_config():
     """
@@ -133,6 +135,21 @@ def modify_tags(arn,tagkey,tagvalue):
     )
 
 
+def get_service_name(color):
+    return f"{config['project-name']}-{color}"
+
+
+def get_current_color():
+    elbname = config['elb-name']
+    state = get_target_groups(elbname)
+
+    for color in ['blue', 'green']:
+        if state['live']['target-group-arn'] == config[color]['target-group-arn']:
+            return color
+
+    raise RuntimeError('Live environment color identification failed')
+
+
 @click.group()
 @click.option('--debug', default=False)
 def cli(debug):
@@ -159,21 +176,126 @@ def deploy(version):
 
     color = "green" if get_current_color() == "blue" else "blue"
 
-    cmd = f"ecs-cli compose --project-name {config['project-name']}-{color} --file={config['compose-file']} --cluster {config['cluster']} --region {config['region']} service up --deployment-max-percent=100 --deployment-min-healthy-percent=0"
+    cmd = f"ecs-cli compose --project-name {get_service_name(color)} --file={config['compose-file']} --cluster {config['cluster']} --region {config['region']} service up --deployment-max-percent=100 --deployment-min-healthy-percent=0"
     click.secho(f"VERSION={version} {cmd}", fg='green')
     subprocess.run(cmd, shell=True, env=dict(os.environ, VERSION=version)).check_returncode()
 
     # TODO: Implement target group health checking
 
-def get_current_color():
-    elbname = config['elb-name']
-    state = get_target_groups(elbname)
 
-    for color in ['blue', 'green']:
-        if state['live']['target-group-arn'] == config[color]['target-group-arn']:
-            return color
+@cli.command()
+def status():
 
-    raise RuntimeError('Live environment color identification failed')
+    response = elbclient.describe_load_balancers(
+        Names=[
+            config['elb-name'],
+        ],
+    )
+
+    elb_dns = response['LoadBalancers'][0]['DNSName']
+
+    services_records = []
+    tasks_records = []
+
+    for color in ["blue", "green"]:
+        response = ecsclient.describe_services(
+            cluster=config['cluster'],
+            services=[
+                get_service_name(color),
+            ]
+        )
+
+        service = response['services'][0]
+
+        task_definition = ecsclient.describe_task_definition(
+            taskDefinition=service['taskDefinition']
+        )
+
+        version = "unknown"
+        for container in task_definition['taskDefinition']['containerDefinitions']:
+            # TODO: Make 'web' container name configurable.
+            if (container['name'] == "web"):
+                version = container['image'].split(":")[1]
+
+        services_records.append({
+            "name": service["serviceName"],
+            "desiredCount": service["desiredCount"],
+            "runningCount": service["runningCount"],
+            "version": version,
+        })
+    
+        response = ecsclient.list_tasks(
+            cluster=config['cluster'],
+            serviceName=get_service_name(color),
+        )
+    
+        task_arns = response['taskArns']
+
+        tasks_list = ecsclient.describe_tasks(
+            cluster=config['cluster'],
+            tasks=task_arns
+        )
+
+
+        for task in tasks_list['tasks']:
+
+            container_instances = ecsclient.describe_container_instances(
+                cluster=config['cluster'],
+                containerInstances=[
+                    task['containerInstanceArn'],
+                ]
+            )
+
+            containerInstance = container_instances['containerInstances'][0]
+
+            instances = ec2client.describe_instances(
+                InstanceIds=[
+                    containerInstance['ec2InstanceId'],
+                ],
+            )
+
+            instance = instances['Reservations'][0]['Instances'][0]
+
+            task_record = {
+                'taskArn': task['taskArn'],
+                'taskId': task['taskArn'].split('/')[1],
+                'service': task['group'].split(":")[1],
+                'lastStatus': task['lastStatus'],
+                'desiredStatus': task['desiredStatus'],
+                'instanceID': containerInstance['ec2InstanceId'],
+                'instanceIP': instance['PrivateIpAddress']
+            }
+            task_record['containers'] = []
+            for container in task['containers']:
+                task_record['containers'].append({
+                    "name": container['name'],
+                    "lastStatus": container['lastStatus'],
+                })
+
+            tasks_records.append(task_record)
+
+
+
+
+    print("Current active deployment: " + get_current_color())
+    print("ELB DNS: " + elb_dns)
+
+    print()
+    format_string = "{:<30} {:<13} {:<13} {:<15}"
+
+    print(format_string.format('Service', 'Desired Count', 'Running Count', 'Version'))
+    for v in services_records:
+        print(format_string.format(v['name'], v['desiredCount'], v['runningCount'], v['version']))
+
+    print()
+    format_string = "{:<38} {:<26} {:<11} {:<14} {:<13} {:<20}"
+
+    print(format_string.format('Task ID', 'Service', 'Last Status', 'Desired Status', 'Instance IP', 'Container Status'))
+    for v in tasks_records:
+        containerStatus = []
+        for container in v['containers']:
+            containerStatus.append(container['name'] + ':' + container['lastStatus'])
+        print(format_string.format(v['taskId'], v['service'], v['lastStatus'], v['desiredStatus'], v['instanceIP'], "; ".join(containerStatus)))
 
 
 if __name__ == "__main__":
